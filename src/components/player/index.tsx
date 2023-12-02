3fec2525270aec6441c909bdb79b064404c8b81e
iamrealwilson <wilsonmarasigan@aol.com>
2023-12-02 12:36:45 +0800

    ....

--- src/components/player/index.tsx
+++ src/components/player/index.tsx
@@ -2,14 +2,7 @@ import React from 'react';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { Container, Overlay, Details, Title, Video, Back } from './styles/player';
 import ReactPlayer from 'react-player';
-import { ShowOverview, ComponentProp } from '../../constants/types';
-
-type VideoProp = {
-	playerRef: React.RefObject<ReactPlayer>;
-	playingDetails: ShowOverview;
-	setIsPaused: boolean;
-	[x: string]: any;
-};
+import { ComponentProp } from '../../constants/types';
 
 function Player({ children, ...restProps }: ComponentProp) {
 	return <Container {...restProps}>{children}</Container>;
@@ -36,12 +29,14 @@ Player.Title = function PlayerTitle({ children, ...restProps }: ComponentProp) {
 };
 
 Player.Video = function PlayerVideo(
-	{ playingDetails, setIsPaused, ...restProps }: VideoProp) 
+	{ playingDetails, setIsPaused, ...restProps }: ComponentProp) 
 {
+    console.log(playingDetails);
+	console.log("**"+restProps);
 	return (
 		<Video {...restProps}>
 			<ReactPlayer
-				url={`https://www.youtube.com/watch?v=${playingDetails.url}`}
+				url={`https://www.youtube.com/watch?v=5ju_BUye0l8`}
 				className="playing-video"
 				width="100%"
 				height="100%"
