7ed92225b7415f637ad5c05850a0475cfab54330
iamrealwilson <wilsonmarasigan@aol.com>
2023-12-03 18:39:32 +0800

    ...

--- src/containers/footer.tsx
+++ src/containers/footer.tsx
@@ -13,7 +13,7 @@ function FooterContainer() {
 					</Footer.MenuItem>
 				))}
 			</Footer.Menu>
-			<Footer.Text>Roseflix Philippines</Footer.Text>
+			<Footer.Text>Legal Lex Philippines</Footer.Text>
 		</Footer>
 	);
 }
