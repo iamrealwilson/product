21f33bfea47d453225c027cdd51b15054ed13e0b
iamrealwilson <wilsonmarasigan@aol.com>
2024-05-27 10:20:12 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -21,6 +21,12 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})
 				 :
+				 userDetails != null && (userDetails.email == "jbquibolmba@gmail.com")
+				 ?
+				 SECTIONS[category].sections.filter(section => section.title.includes("Political")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+							})
+				 :
 				 userDetails != null && (userDetails.email == "emil@law.com")
 				 ?
 				 SECTIONS[category].sections.filter(section => section.title.includes("Criminal")).map((section, i) => {
@@ -30,6 +36,12 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 				userDetails != null && (userDetails.email == "espellargamae@gmai.com")
 				?
 				SECTIONS[category].sections.filter(section => section.title.includes("Taxation")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+									   })		
+				:
+				userDetails != null && (userDetails.email == "rosenclaires@gmail.com")
+				?
+				SECTIONS[category].sections.filter(section => section.title.includes("Civil")).map((section, i) => {
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 									   })			
 			     :
