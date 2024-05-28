8c80347f2483a6c359c5de023a1850dbed64814a
iamrealwilson <wilsonmarasigan@aol.com>
2024-05-28 08:29:53 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -21,6 +21,18 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})
 				 :
+				 userDetails != null && (userDetails.email == "estellegayecalantasbueno@gmail.com")
+				 ?
+				 SECTIONS[category].sections.filter(section => section.title.includes("Taxation") || section.title.includes("Civil") || section.title.includes("Political") || section.title.includes("Remedial")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+							})
+ 				 :
+				 userDetails != null && (userDetails.email == "lai.fesalbon@gmail.com")
+				 ?
+				 SECTIONS[category].sections.filter(section => section.title.includes("Criminal") || section.title.includes("Remedial")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+							})
+				 :
 				 userDetails != null && (userDetails.email == "jbquibolmba@gmail.com")
 				 ?
 				 SECTIONS[category].sections.filter(section => section.title.includes("Political")).map((section, i) => {
