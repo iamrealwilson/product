85ee584b567f0d5bcb0487015fd89b268f9af30e
iamrealwilson <wilsonmarasigan@aol.com>
2024-06-05 11:03:03 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -93,12 +93,6 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})			
 				 :
-				 userDetails != null && (userDetails.email == "jbquibolmba@gmail.com")
-				 ?
-				 SECTIONS[category].sections.filter(section => section.title.includes("Political")).map((section, i) => {
-								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
-							})
-				 :
 				 userDetails != null && (userDetails.email == "emil@law.com")
 				 ?
 				 SECTIONS[category].sections.filter(section => section.title.includes("Criminal")).map((section, i) => {
