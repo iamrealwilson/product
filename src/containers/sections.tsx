d688492f85290f2f487cdf92d651d4d73b46c6a0
iamrealwilson <wilsonmarasigan@aol.com>
2024-06-08 14:29:30 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -27,15 +27,15 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})
 			     :
-				 userDetails != null && (userDetails.email == "kmadrid1951@gmail.com")
-				 ?
-				 SECTIONS[category].sections.filter(section => section.title.includes("Labor") || section.title.includes("Commercial") || section.title.includes("Criminal") || section.title.includes("Taxation") || section.title.includes("Civil") || section.title.includes("Political") || section.title.includes("Remedial")).map((section, i) => {
-								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
-							})
-				 :
 				 userDetails != null && (userDetails.email == "lexlibertatis8888@gmail.com")
 				 ?
 				 SECTIONS[category].sections.filter(section => section.title.includes("Commercial") || section.title.includes("Criminal") || section.title.includes("Taxation") || section.title.includes("Political") || section.title.includes("Remedial")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+							})
+				 :
+				 userDetails != null && (userDetails.email == "kmadrid1951@gmail.com")
+				 ?
+				 SECTIONS[category].sections.filter(section => section.title.includes("Labor") || section.title.includes("Commercial") || section.title.includes("Criminal") || section.title.includes("Taxation") || section.title.includes("Civil") || section.title.includes("Political") || section.title.includes("Remedial")).map((section, i) => {
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})
 				 :
