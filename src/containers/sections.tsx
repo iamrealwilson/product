c49627d4521e538444f74b448371a332d1867917
iamrealwilson <wilsonmarasigan@aol.com>
2024-06-21 20:53:36 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -125,7 +125,7 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
  				 :
 				 userDetails != null && (userDetails.email == "roy.ladiasan@gmail.com")
 				 ?
-				 SECTIONS[category].sections.filter(section => section.title.includes("Remedial") || section.title.includes("Civil") || section.title.includes("Political") || section.title.includes("Criminal")).map((section, i) => {
+				 SECTIONS[category].sections.filter(section => section.title.includes("Taxation") || section.title.includes("Remedial") || section.title.includes("Civil") || section.title.includes("Political") || section.title.includes("Criminal")).map((section, i) => {
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							}) 		
  				 :
