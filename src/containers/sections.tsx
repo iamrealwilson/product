1a60ef0e2afa1ce522fd083d661d77ffae7f8f0e
markabongski <markabongski@gmail.com>
2024-06-28 20:07:24 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -27,6 +27,12 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})	
 				 :
+				 userDetails != null && (userDetails.email == "rjtravilla@yahoo.com")
+				 ?
+				 SECTIONS[category].sections.filter(section => section.title.includes("Political") || section.title.includes("Labor") ||  section.title.includes("Criminal") || section.title.includes("Civil")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+							})
+				 :
 				 userDetails != null && (userDetails.email == "bautistajoel316@gmail.com")
 				 ?
 				 SECTIONS[category].sections.filter(section => section.title.includes("Civil") || section.title.includes("Remedial") ||  section.title.includes("Criminal")).map((section, i) => {
