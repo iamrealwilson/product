422734348eac1bb81911cb973b65515d482aa409
iamrealwilson <wilsonmarasigan@aol.com>
2024-07-22 11:04:31 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -23,7 +23,7 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 				 :
 				 userDetails != null && (userDetails.email == "ur_interpreter83#@yahoo.com")
 				 ?
-				 SECTIONS[category].sections.filter(section => section.title.includes("Preweek")).map((section, i) => {
+				 SECTIONS[category].sections.filter(section => section.title.includes("Criminal") || section.title.includes("Political") || section.title.includes("Labor") || section.title.includes("Preweek")).map((section, i) => {
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})	
 				 :
