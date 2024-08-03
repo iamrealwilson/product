6c47cc8ea12e542010bb4e12515c2e307489dda6
markabongski <markabongski@gmail.com>
2024-08-03 12:56:27 +0800

    ..

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -27,6 +27,18 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})
 				 :
+				 userDetails != null && (userDetails.email === "preweek@39")
+				 ?
+				 SECTIONS[category].sections.filter(section => section.title.includes("Preweek - Criminal") || section.title.includes("Preweek - Commercial") || section.title.includes("Preweek - Remedial")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+							})
+				 :
+				 userDetails != null && (userDetails.email === "preweek@40")
+				 ?
+				 SECTIONS[category].sections.filter(section => section.title.includes("Preweek - Criminal") || section.title.includes("Preweek - Taxation") || section.title.includes("Preweek - Civil") || section.title.includes("Preweek - Commercial") || section.title.includes("Preweek - Labor")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+							})
+				 :
 				 userDetails != null && (userDetails.email === "preweek@1")
 				 ?
 				 SECTIONS[category].sections.filter(section => section.title.includes("Preweek - Taxation") || section.title.includes("Preweek - Commercial") || section.title.includes("Preweek - Criminal")).map((section, i) => {
