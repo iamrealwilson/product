c9c99be9aea732f84272574e3e3834993c79ddcd
markabongski <markabongski@gmail.com>
2024-08-04 12:52:10 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -33,6 +33,12 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})
 				 :
+				 userDetails != null && (userDetails.email === "apbballesteros@gmail.com")
+				 ?
+				 SECTIONS[category].sections.filter(section => section.title.includes("Taxation") || section.title.includes("Remedial") || section.title.includes("Criminal")).map((section, i) => {
+								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
+							})
+				 :
 				 userDetails != null && (userDetails.email === "preweek@39")
 				 ?
 				 SECTIONS[category].sections.filter(section => section.title.includes("Preweek - Criminal") || section.title.includes("Preweek - Commercial") || section.title.includes("Preweek - Remedial")).map((section, i) => {
