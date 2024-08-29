6fb03b266476ad4ada24a7add8dc448f4c553593
iamrealwilson <wilsonmarasigan@aol.com>
2024-08-29 21:29:38 +0800

    ...

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://ee-ws-api.onrender.com/api/v3'
+  baseURL: 'https://e-tmdb-api.onrender.com/api/v3'
 });
 
 export default instance;
