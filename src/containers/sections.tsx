9d097c70ebe976e51a50332ec95e3b2c60834396
iamrealwilson <wilsonmarasigan@aol.com>
2024-09-10 21:33:44 +0800

    ...

--- src/containers/sections.tsx
+++ src/containers/sections.tsx
@@ -67,7 +67,7 @@ function SectionsContainer({ userDetails, category, sectionDisplayed }: Sections
 				 userDetails.email === "lmt36@law.com" 
 				 )
 				 ?
-				 SECTIONS[category].sections.filter(section => section.title.includes("Last Minute")).map((section, i) => {
+				 SECTIONS[category].sections.filter(section => section.title.includes("Last Minute Tip - Speed Lecture")).map((section, i) => {
 								return i < sectionDisplayed && <SliderContainer key={section.title} section={section} />;
 							})	
 				 :
